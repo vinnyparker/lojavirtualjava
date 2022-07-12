76db6ffb379cda54c3382999d846341c8bb72d3d
= <vinny.parker@gmail.com>
2022-07-12 12:51:10 -0300

    start project

---
+++ src/main/java/br/com/lojavirtual/LojavirtualJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.lojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojavirtualJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LojavirtualJavaApplication.class, args);
+    }
+
+}
